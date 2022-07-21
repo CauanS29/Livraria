ac9ef578dc9bc2e6d485116af819f022a71f6610
Cauan Santos <cauansantos043@gmail.com>
2022-07-20 21:28:48 -0300

    Projeto Livraria

---
+++ src/br/com/casadocodigo/livraria/produtos/Produto.java
@@ -0,0 +1,7 @@
+package br.com.casadocodigo.livraria.produtos;
+
+public interface Produto extends Comparable<Produto>{
+    //Os metodos sao publicos e abstratos por padrao
+    //public abstract double getValor();
+    double getValor();
+}
---
+++ src/br/com/casadocodigo/livraria/teste/RegistroDeVendas.java
@@ -0,0 +1,40 @@
+package br.com.casadocodigo.livraria.teste;
+
+import br.com.casadocodigo.livraria.Autor;
+import br.com.casadocodigo.livraria.CarrinhoDeCompra;
+import br.com.casadocodigo.livraria.produtos.Ebook;
+import br.com.casadocodigo.livraria.produtos.LivroFisico;
+import br.com.casadocodigo.livraria.produtos.Produto;
+
+import java.util.List;
+
+public class RegistroDeVendas {
+    public static void main(String[] args) {
+        Autor autor = new Autor();
+        autor.setNome("Mauricio Aniche");
+
+        LivroFisico livroFisico = new LivroFisico(autor);
+        livroFisico.setNome("Test-Driven Development");
+        livroFisico.setValor(59.90);
+
+        Ebook ebook = new Ebook(autor);
+        ebook.setNome("Test-Driven Development");
+        ebook.setValor(29.90);
+
+        if(livroFisico.aplicaDescontoDe10Porcento()) {
+            System.out.println("Valor agora é: " + livroFisico.getValor());
+        }
+
+        CarrinhoDeCompra carrinhoDeCompra = new CarrinhoDeCompra();
+        carrinhoDeCompra.adiciona(livroFisico);
+        carrinhoDeCompra.adiciona(ebook);
+        System.out.println("Total: " + carrinhoDeCompra.getTotal());
+
+        List<Produto> produtos = carrinhoDeCompra.getProdutos();
+        for(Produto produto : produtos) {
+            System.out.println(produto.getValor());
+        }
+
+        System.out.println("Fui executado!");
+    }
+}
