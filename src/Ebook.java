c0cbff746ba749e6c892f64498645ff1b5741b69
Cauan Santos <cauansantos043@gmail.com>
2022-07-25 09:47:10 -0300

    Add Livraria Project

---
+++ src/Ebook.java
@@ -0,0 +1,23 @@
+public class Ebook extends Livro {
+
+    private String waterMark;
+
+    public Ebook (Autor autor) {
+        super(autor);
+    }
+
+    public void setWaterMark(String waterMark) {
+        this.waterMark = waterMark;
+    }
+
+    @Override
+    public boolean aplicaDescontoDe(double porcentagem) {
+        if (porcentagem > 0.15){
+            return false;
+        }
+        return super.aplicaDescontoDe(porcentagem);
+    }
+
+
+}
+
