eefc612b472a4cdb26b32a4521265d84b13cfd5d
Cauan Santos <cauansantos043@gmail.com>
2022-07-25 09:44:13 -0300

    Delete project

--- src/br/com/casadocodigo/livraria/produtos/Produto.java
+++
@@ -1,7 +0,0 @@
-package br.com.casadocodigo.livraria.produtos;
-
-public interface Produto extends Comparable<Produto>{
-    //Os metodos sao publicos e abstratos por padrao
-    //public abstract double getValor();
-    double getValor();
-}
